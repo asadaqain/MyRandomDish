6d792c09237f52b8c4dde39c20b2a74df00553bc
asadaqain <sadaqain@yorku.ca>
2020-02-06 16:29:17 -0500

    init with card

---
+++ app/src/main/java/myrandomdish/com/MainActivity.kt
@@ -0,0 +1,16 @@
+package myrandomdish.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dish_name.text = "Default Dish"
+        dish_description.text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+    }
+}
